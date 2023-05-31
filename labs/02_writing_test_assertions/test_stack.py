bea8e162f5fc367de0098a17e4afc654484dd1bd
Constantine <blackphoenix343@gmail.com>
2023-05-31 22:26:16 +0200

    Updates on tests
    for the IBM coutse

--- labs/02_writing_test_assertions/test_stack.py
+++ labs/02_writing_test_assertions/test_stack.py
@@ -14,16 +14,29 @@ class TestStack(TestCase):
 
     def test_push(self):
         """Test pushing an item into the stack"""
-        raise Exception("not implemented")
+        self.stack.push(1)
+        self.assertEqual(self.stack.peek(), 1)
+        self.stack.push(2)
+        self.assertEqual(self.stack.peek(), 2)
 
     def test_pop(self):
         """Test popping an item of off the stack"""
-        raise Exception("not implemented")
+        self.stack.push(1)
+        self.stack.push(2)
+        self.assertEqual(self.stack.pop(), 2)
+        self.assertEqual(self.stack.peek(), 1)
 
     def test_peek(self):
         """Test peeking at the top the stack"""
-        raise Exception("not implemented")
+        self.stack.push(89)
+        self.assertEqual(self.stack.peek(), 89)
+        self.stack.push(45)
+        self.assertEqual(self.stack.peek(), 45)
+
 
     def test_is_empty(self):
         """Test if the stack is empty"""
-        raise Exception("not implemented")
+        self.assertEqual(self.stack.is_empty(), 1)
+        self.stack.push(5)
+        self.assertEqual(self.stack.is_empty(), 0)
+
